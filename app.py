dda480e6502a93c89dee94fd8f6b996ebd657c34
ozgehalilduman <ozgehalilduman@gmail.com>
2025-09-01 21:41:49 +0300

    açıklama satırları ve gereksiz deneme satırlarından temizlendi

--- app.py
+++ app.py
@@ -32,6 +32,8 @@ class MyWindow(QMainWindow, Ui_Form):
         LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
         LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
         
+        SEGMENT_LED_SAYISI=4 # 7 segmentlı yapıda bir segmente bulunan led sayısı
+        self.SEGMENT_LED_RENGI=Color(255,0,0) # scoreBoard un genel rengi
         # Create NeoPixel object with appropriate configuration.
         self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
         # Intialize the library (must be called once before other functions).
@@ -44,14 +46,20 @@ class MyWindow(QMainWindow, Ui_Form):
         # Guest skor nesneleri
         self.guest_skor = ScoreBoard_Ayarla([0, 0], 4)
         self.guest_skor_ayarla = Skor_Ayarla([0, 0], 0, 100)
-
+        """
+        Bir takımın 2 haneden oluşan skoru (00..99) olduğundan
+        skoru oluşturan herbir rakamın Şerit LED üzerinde kaç led ile
+        ifade edilğini ayarllıyorum. bu örnekte 7 segmenten oluşan rakamın
+        her segmentinde 4 led olduğundan 7x4=28, eğer 5 led den oluşsaydı 7x5=35 olacaktı
+        """
+        sg_led=SEGMENT_LED_SAYISI*7
         # Home skor LED bağlantıları
-        self.home_skor.led_ekle([i for i in range(28)])
-        self.home_skor.led_ekle([i for i in range(28,57)])
+        self.home_skor.led_ekle([i for i in range(sg_led)])
+        self.home_skor.led_ekle([i for i in range(sg_led,sg_led*2)])
 
         # Guest skor LED bağlantıları
-        self.guest_skor.led_ekle([i for i in range(57,85)])
-        self.guest_skor.led_ekle([i for i in range(85,113)])
+        self.guest_skor.led_ekle([i for i in range(sg_led*2,sg_led*3)])
+        self.guest_skor.led_ekle([i for i in range(sg_led*3,sg_led*4)])
 
         # Buton bağlantıları (dinamik hale getirildi)
         self.btn_homeskor_azalt3.clicked.connect(
@@ -97,39 +105,38 @@ class MyWindow(QMainWindow, Ui_Form):
         self.rakam_goster(self.home_skor, [0, 0])
         self.rakam_goster(self.guest_skor, [0, 0])
     
+    """
+    _skor_ayarla hangi takımın       skorunu belirtilen oranda arttırıp,
+    azaltmayı sağlar
+    """
     def sayac_ayarla(self, _skor_ayarla, _skor, durum, oran):
         """ Skoru güncelle ve LED’lere yansıt """
         _skor_ayarla.sayi_ayarla(durum, oran)
         self.rakam_goster(_skor, _skor_ayarla.skor_Goster())
 
-        
+    """ _skor ile belirtilen takımın skorunun rakam ile belirtilen değere getiri"""   
     def rakam_goster(self, _skor, rakam):
-        '''self.strip.setPixelColor(119,Color(0,255,0))
-        self.strip.setPixelColor(113,Color(0,0,255))
-        self.strip.show()
-        time.sleep(2000/1000)'''
-        print(_skor.leds)
         # Önce tüm LED'leri kapat
         for i in range(_skor.digit_sayisi()):# kaç adet takımın skoru tutuluyorsa
             for k in _skor.leds[i]:
-                #print("{0}-{1}".format(i,k))
                 self.strip.setPixelColor(k,Color(0,0,0))
                 self.strip.show()
             #time.sleep(1/1000)
                 
-
+        """Arayüzdeki Skorları değiştirmemi sağlıyor"""
         if _skor==self.home_skor:
             self.label_homeskor.setText(str(rakam[0]) + str(rakam[1]))
         else:
             self.label_guestskor.setText(str(rakam[0]) + str(rakam[1]))
-        # İlgili rakamları yak
-        
+        """
+            _skor ile belirtilen takım için, skoru oluşturan rakamlar için teker teker,
+            ilgili rakamı oluşturmak için hangi indexde bulunan ledleri yakması gerektiğini
+            belirleyerek ilgili takımın led tablosundan o indexde bulunan led numarasına yanma
+            komutu verir
+        """
         for i in range(_skor.digit_sayisi()):# kaç adet takımın skoru tutuluyorsa
-            #print(_skor.rakams[rakam[i]])
             for k in _skor.rakams[rakam[i]]:
-                if _skor.leds[i][k]==55:
-                    print('55 nolu led {0}-{1}'.format(i,k))
-                self.strip.setPixelColor(_skor.leds[i][k],Color(255,0,0))
+                self.strip.setPixelColor(_skor.leds[i][k],self.SEGMENT_LED_RENGI)
                 self.strip.show()
             
 
